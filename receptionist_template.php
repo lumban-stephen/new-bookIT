e819a7f9544025963a469aa7cf2bf4f80439f90f
stpn-lmbn <70630241+stpn-lmbn@users.noreply.github.com>
2020-12-01 12:46:40 +0800

    Add files
    Preliminary files added

---
+++ receptionist_template.php
@@ -0,0 +1,36 @@
+<?php
+   session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Receptionist Template</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <header>
+        <img>
+        <div>
+            <img>
+            <p>Welcome, </p>
+            <a><img></img></a>
+        </div>
+        </header>
+        <nav><ul>
+                <li><a>Dashboard</a></li>
+                <li><a>Check In</a></li>
+                <li><a>Check Out</a></li>
+                <li><a>Reservation</a></li>
+                <li><a>Records</a></li>
+                <li><a>To Do List</a></li>
+                <li><a>Guests</a></li>
+            </ul>
+        </nav>
+        <div id="content">
+        
+        </div>
+    </body>
