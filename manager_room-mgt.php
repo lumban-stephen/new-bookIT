9bef8ca07e7b0347d409fd27e54d3cb365174b91
stpn-lmbn <70630241+stpn-lmbn@users.noreply.github.com>
2020-12-03 17:00:53 +0800

    Add manager pages
    All manager pages are added.

---
+++ manager_room-mgt.php
@@ -0,0 +1,35 @@
+<?php
+   session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>BookIT</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <header>
+        <img>
+        <div>
+            <img>
+            <p>Welcome, </p>
+            <a><img></img></a>
+        </div>
+        </header>
+        <nav><ul>
+                <li><a>Dashboard</a></li>
+                <li><a>Revenue</a></li>
+                <li><a>Records</a></li>
+                <li><a>Guests</a></li>
+                <li><a>Room Management</a></li>
+                <li><a>Staff Management</a></li>
+            </ul>
+        </nav>
+        <div id="content">
+            <!--Code here for manager room management code-->
+        </div>
+    </body>
