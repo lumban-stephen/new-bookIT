0bb151994a7e2e43551f4c43a125bd4b151b9b5a
stpn-lmbn <70630241+stpn-lmbn@users.noreply.github.com>
2020-12-03 16:52:11 +0800

    Add reception pages
    All reception pages are added

---
+++ receptionist_res-list.php
@@ -0,0 +1,39 @@
+<?php
+   session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>BookIT</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <header>
+        <div id="header">
+        <img src="assets/bookIT_Logo.png">
+            <div class="right-float">
+                <img>
+                <p>Welcome, </p>
+                <a><img></img></a>
+            </div>
+        </div>
+        </header>
+        <nav><ul>
+                <li><a>Dashboard</a></li>
+                <li><a>Check In</a></li>
+                <li><a>Check Out</a></li>
+                <li><a>Reservation</a></li>
+                <li><a>Records</a></li>
+                <li><a>To Do List</a></li>
+                <li><a>Guests</a></li>
+            </ul>
+        </nav>
+        <div id="content">
+            <!--Code Here only-->
+            <!--Reservation list page code in here-->
+        </div>
+    </body>
