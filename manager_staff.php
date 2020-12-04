880b052cd1a03ce8ce6d87c02e0a31b11abde7a7
stpn-lmbn <70630241+stpn-lmbn@users.noreply.github.com>
2020-12-04 08:55:37 +0800

    Create manager_staff.php
    new manager file

---
+++ manager_staff.php
@@ -0,0 +1,35 @@
+<?php
+   session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>BookIT</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <header>
+        <img>
+        <div>
+            <img>
+            <p>Welcome, </p>
+            <a><img></img></a>
+        </div>
+        </header>
+        <nav><ul>
+                <li><a href="manager_dashboard.php">Dashboard</a></li>
+                <li><a href="manager_revenue.php">Revenue</a></li>
+                <li><a href="manager_records.php">Records</a></li>
+                <li><a href="manager_guests.php">Guests</a></li>
+                <li><a href="manager_room-mgt.php">Room Management</a></li>
+                <li><a href="#">Staff Management</a></li>
+            </ul>
+        </nav>
+        <div id="content">
+            <!--Code here for manager_staff-->
+        </div>
+    </body>
