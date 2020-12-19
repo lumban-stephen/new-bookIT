204500eb4ae85ba93ce3ca466199723a34198666
stpn-lmbn <70630241+stpn-lmbn@users.noreply.github.com>
2020-12-19 21:10:02 +0800

    Update staff-server.php
    delete button is now working

--- staff-server.php
+++ staff-server.php
@@ -1,5 +1,6 @@
 <?php
     include 'connection.php';
+    session_start();
 
     if(isset($_POST['save'])){
         $fname = $_POST['fname'];
@@ -19,6 +20,12 @@
     }
 
     if(isset($_GET['delete'])){
+        $id = $_GET['delete'];
+        
+        $deleteQuery = "DELETE FROM users WHERE user_id=$id";
 
+        $conn->query($deleteQuery) or die($conn->error);
+
+        header('location: manager_staff.php');
     }
 ?>
