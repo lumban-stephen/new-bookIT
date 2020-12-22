0513460a55568cfede8d5c608dac9fb854590bbd
joshuakuizon2519 <19100196@usc.edu.ph>
2020-12-22 12:57:22 +0800

    Updated receptionist dashboard

---
+++ dashmodal.js
@@ -0,0 +1,71 @@
+document.getElementById('Modal1').addEventListener('click',function() {
+document.querySelector('.bg-modal1').style.display = 'flex';
+            });
+document.querySelector('.close1').addEventListener('click',function(){
+document.querySelector('.bg-modal1').style.display = 'none';
+            });
+document.getElementById('Modal2').addEventListener('click',function() {
+document.querySelector('.bg-modal2').style.display = 'flex';
+            });
+document.querySelector('.close2').addEventListener('click',function(){
+document.querySelector('.bg-modal2').style.display = 'none';
+            });
+document.getElementById('Modal3').addEventListener('click',function() {
+document.querySelector('.bg-modal3').style.display = 'flex';
+             });
+document.querySelector('.close3').addEventListener('click',function(){
+document.querySelector('.bg-modal3').style.display = 'none';
+            });
+document.getElementById('Modal4').addEventListener('click',function() {
+document.querySelector('.bg-modal4').style.display = 'flex';
+            });
+document.querySelector('.close4').addEventListener('click',function(){
+document.querySelector('.bg-modal4').style.display = 'none';
+            });
+document.getElementById('Modal5').addEventListener('click',function() {
+document.querySelector('.bg-modal5').style.display = 'flex';
+            });
+document.querySelector('.close5').addEventListener('click',function(){
+document.querySelector('.bg-modal5').style.display = 'none';
+            });  
+                    
+/** Date and Time
+ --------------------------------------*/
+ function updateClock(){
+    var now = new Date();
+    var dname = now.getDay(),
+        mo = now.getMonth(),
+        dnum = now.getDate(),
+        yr = now.getFullYear(),
+        hou = now.getHours(),
+        min = now.getMinutes(),
+        sec = now.getSeconds(),
+        pe = "AM";
+        
+        if(hou >= 12){
+          pe = "PM";
+        }
+        if(hou == 0){
+          hou = 12;
+        }
+        if(hou > 12){
+          hou = hou - 12;
+        }
+
+        Number.prototype.pad = function(digits){
+          for(var n = this.toString(); n.length < digits; n = 0 + n);
+          return n;
+        }
+
+        var months = ["January", "February", "March", "April", "May", "June", "July", "Augest", "September", "October", "November", "December"];
+        var week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+        var ids = ["dayname", "month", "daynum", "year", "hour", "minutes", "seconds", "period"];
+        var values = [week[dname], months[mo], dnum.pad(2), yr, hou.pad(2), min.pad(2), sec.pad(2), pe];
+        for(var i = 0; i < ids.length; i++)
+        document.getElementById(ids[i]).firstChild.nodeValue = values[i];
+  }
+
+  function initClock(){
+    updateClock();
+    window.setInterval("updateClock()", 1);
+  }
--- receptionist_dashboard.php
+++ receptionist_dashboard.php
@@ -10,40 +10,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>BookIT</title>
         <link rel="stylesheet" href="style.css">
-        <style>
-            .bg-modal {
-    width: 100%;
-    height: 100%;
-    background-color: rgba(0, 0, 0, 0.5);
-    position: absolute;
-    top: 0;
-    left: 0;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    display: none; 
-    margin-bottom: 0;
-}
-.modal-content {
-    width: 400px;
-    height: 400px;
-    background-color: white;
-    border-radius: 4px;
-    text-align: center;
-    position: relative;
-}
-.close{
-    position: absolute;
-    top: 0;
-    right: 14px;
-    font-size: 42px;
-    transform: rotate(45deg);
-    cursor: pointer;
-
-}
-        </style>
     </head>
-    <body>
+    <body onload="initClock()">
         <header>
         <div id="header">
         <img src="assets/bookIT_Logo.png">
@@ -81,8 +49,8 @@
         <h2>Dashboard</h2>
         <br><br>
         <div class="dashwrapper">
-                <div class="dash box1" id="Modal">
-                    <?php
+            <div class="dash box1" id="Modal1">
+                     <?php
                         include 'connection.php';
         
                         $sql = "SELECT COUNT(*) AS 'Guests'
@@ -96,14 +64,21 @@
                             }else{
                               echo "No guest checked in";
                             }     
+
                     ?>
-                   <div class="bg-modal">
-                        <div class="modal-content">
-                            <div class="close">+</div>
-                            <div class="header">s
-                                <h1>Modal</h1>
-                                </div>
-                                <?php
+            </div>
+            <div class="bg-modal1">
+            <div class="modal-content1">
+            <div class="close1">+</div>
+            <div class="header_modal1">
+                        <h1>Guests checked in</h1>
+                        </div><br>
+                        <table id="Table">
+                        <tr>
+                            <th>Guest Name</th>
+                            <th>Room Number</th>
+                        </tr>
+                        <?php
                                 include 'connection.php';
                 
                                 $sql = "SELECT ch.checked_in_id AS 'ID',
@@ -117,11 +92,22 @@
                                                 AND ch.room_id = r.room_id
                                         ORDER BY
                                                 g.date_in;";
+                                        $display = $conn->query($sql);
+                                        if($rows = $display != NULL){ 
+                                            while($rows = $display->fetch_assoc()){
+                                                echo
+                                                   "<tr><td>" .$rows['Guest Name']."</td>
+                                                   <td>" .$rows['Room Number']."</td></tr>";
+                                             }
+                                            }else{
+                                              echo "No guest checked in";
+                                            } 
+                                            echo "</table>" ;  
                                 ?>
                         </div>
-                    </div> 
-                </div>
-                <div class="dash box2" id="Modal"> 
+                        </div>
+
+                <div class="dash box2" id="Modal2"> 
                     <?php
                          include 'connection.php';
         
@@ -137,8 +123,47 @@
                             }else{
                               echo "No guest is about to check-out";
                             }     
-                    ?></div>
-                <div class="dash box3" id="Modal">                    
+                    ?>
+                    </div>
+                    <div class="bg-modal2">
+                    <div class="modal-content2">
+                    <div class="close2">+</div>
+                    <div class="header_modal2">
+                                <h1>About to checkout</h1>
+                                </div><br>
+                                <table id="Table">
+                                <tr>
+                                    <th>Guest Names</th>
+                                    <th>Room Number</th>
+                                </tr>
+                                <?php
+                                        include 'connection.php';
+                        
+                                        $sql = "SELECT CONCAT(c.fname, ' ', c.MI, ' ', c.lname) AS 'Guest Name',
+                                                     r.room_id as 'Room Number'
+                                                FROM
+                                                        Checked_in_guests ch, Guests g, Customers c,
+                                                        Rooms r
+                                                WHERE
+                                                        ch.guest_id = g.guest_id AND g.customer_id = c.customer_id
+                                                        AND ch.room_id = r.room_id AND g.date_out= CURDATE()
+                                                ORDER BY
+                                                        g.date_in;";
+                                                $display = $conn->query($sql);
+                                                if($rows = $display != NULL){ 
+                                                    while($rows = $display->fetch_assoc()){
+                                                        echo
+                                                        "<tr><td>" .$rows['Guest Name']."</td>
+                                                        <td>" .$rows['Room Number']."</td></tr>";
+                                                    }
+                                                    }else{
+                                                    echo "No guest is going to checkout";
+                                                    } 
+                                                    echo "</table>" ;  
+                                        ?>
+                        </div>
+                        </div>
+                <div class="dash box3" id="Modal3">                    
                     <?php
                          include 'connection.php';
         
@@ -155,7 +180,38 @@
                               echo "No available rooms";
                             }     
                     ?></div>
-                <div class="dash box4" id="Modal">
+                    <div class="bg-modal3">
+                    <div class="modal-content3">
+                    <div class="close3">+</div>
+                    <div class="header_modal3">
+                                <h1>Vacant Rooms</h1>
+                                </div><br>
+                                <table id="Table">
+                                <tr>
+                                    <th>Room ID</th>
+                                    <th>Room Description</th>
+                                </tr>
+                                <?php
+                                        include 'connection.php';
+                        
+                                        $sql = "SELECT r.room_id AS 'Room ID', rt.room_desc AS 'Room Description'
+                                        FROM rooms r, room_type rt
+                                        WHERE r.roomtype_id=rt.roomtype_id AND room_status='Available';";
+                                                $display = $conn->query($sql);
+                                                if($rows = $display != NULL){ 
+                                                    while($rows = $display->fetch_assoc()){
+                                                        echo
+                                                        "<tr><td>" .$rows['Room ID']."</td>
+                                                        <td>" .$rows['Room Description']."</td></tr>";
+                                                    }
+                                                    }else{
+                                                    echo "No Vacant rooms";
+                                                    } 
+                                                    echo "</table>" ;  
+                                        ?>
+                        </div>
+                        </div>
+                <div class="dash box4" id="Modal4">
                     <?php
                          include 'connection.php';
         
@@ -173,31 +229,57 @@
                               echo "No reservations";
                             }     
                     ?></div>
-                <div class="dash longbox5" id="Modal">
+                    <div class="bg-modal4">
+                    <div class="modal-content4">
+                    <div class="close4">+</div>
+                    <div class="header_modal4">
+                                <h1>Reservations</h1>
+                                </div><br>
+                                <table id="Table">
+                                <tr>
+                                    <th>Guest name</th>
+                                    <th>Check-in date</th>
+                                </tr>
+                                <?php
+                                        include 'connection.php';
+                        
+                                        $sql = "SELECT CONCAT(c.fname, ' ', c.MI, ' ', c.lname) AS 'Guest Name',
+                                                     g.date_in AS 'Guest in'
+                                                FROM
+                                                        schedule s, Guests g, Customers c
+                                                WHERE
+                                                        s.guest_id = g.guest_id AND s.customer_id = c.customer_id;";
+                                                $display = $conn->query($sql);
+                                                if($rows = $display != NULL){ 
+                                                    while($rows = $display->fetch_assoc()){
+                                                        echo
+                                                        "<tr><td>" .$rows['Guest Name']."</td>
+                                                        <td>" .$rows['Guest in']."</td></tr>";
+                                                    }
+                                                    }else{
+                                                    echo "No Reservations";
+                                                    } 
+                                                    echo "</table>" ;  
+                                        ?>
+                        </div>
+                        </div>
+                <div class="dash longbox5" id="Modal5">
                     <div class="datetime">
                         <div class="date">
-                            <span id="dayname">Day</span>
+                            <span id="dayname">Day</span>,
                             <span id="month">Month</span>
-                            <span id="daynum">00</span>
+                            <span id="daynum">00</span>,
                             <span id="year">Year</span>
                         </div>
                         <div class="time">
                             <span id="hour">00</span>:
                             <span id="minutes">00</span>:
                             <span id="seconds">00</span>
-                            <span id="period">Am</span>
-                        </div> 
-                </div>
+                            <span id="period">AM</span>
+                        </div>
+                        </div>
             </div>
-                
         </div>
-        <script>
-            document.getElementById('Modal').addEventListener('click',function() {
-                document.querySelector('.bg-modal').style.display = 'flex';
-            });
-
-            document.querySelector('.close').addEventListener('click', function(){
-            document.querySelector('.bg-modal-resched').style.display = 'none';
-        });
-        </script>
+        </div>
+        <script src="dashmodal.js"></script>
     </body>
