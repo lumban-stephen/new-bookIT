bad93c857807b7696b3a5f234d5592816af8e86c
ai-ramen <73935314+ai-ramen@users.noreply.github.com>
2020-12-22 09:01:15 +0800

    Update manager_guests.php

--- manager_guests.php
+++ manager_guests.php
@@ -23,7 +23,10 @@
                 </a>
             </div>
             <div class="right-float">
-                <p>Welcome Manager,</p>
+                <p>Welcome,</p>
+            </div>
+            <div class="right-float">
+                <a><img></img></a>
             </div>
         </div>
         </header>
@@ -63,16 +66,17 @@
 
             <?php
         include 'connection.php';
+        ob_start();
         
             $sql = "SELECT s.sched_id AS 'SID',
-                    CONCAT(c.fname, ' ', c.MI, ' ', c.lname) AS 'Guest Name',
-                    r.room_id as 'Room Number'
+                    CONCAT(c.fname, ' ', c.MI, ' ', c.lname) AS 'Guest Name',c.fname as 'fname',c.MI as 'mname',c.lname as 'lname',
+                    r.room_id as 'Room Number', g.guest_id AS 'guest_id',b.bill_id as 'bill_id'
             FROM
                     Schedule s, Guests g, Customers c,
-                    Rooms r
+                    Rooms r,bill b
             WHERE
-                    s.guest_id = g.guest_id AND s.customer_id = c.customer_id
-                    AND s.room_id = r.room_id
+                    s.guest_id = g.guest_id AND g.customer_id = c.customer_id
+                    AND s.room_id = r.room_id AND b.guest_id=g.guest_id
             GROUP BY
                     g.guest_id /*I grouped it by guest id because it would be group by roomtype_id if isnt guest_id*/ 
             ORDER BY
@@ -84,17 +88,70 @@
             if($rows = $display != NULL){ //I didn't put fetch assoc because the first value won't show if the fetch_assoc() is called twice.
                 while($rows = $display->fetch_assoc()){
                     echo
-                        "<tr><td>". $rows['Guest Name']. "</td>
+                        "<form action='' method='POST'>
+                        <tr><td>". $rows['Guest Name']. "</td>
                              <td>". $rows['Room Number']. "</td>
-                             <td><button  class='Offerbutton'><a href='receptionist_ameneties.php'>Offer<br>Amenities</a></button>
-                                 <button class='Extendbutton'>Extend<br>Stay</button>
-                                 <button class='Checkoutbutton'>Early<br>Checkout</button></td>
-                                 <td><button class= 'Viewbutton'><a href= 'receptionist_guestview.php?id=".$rows['SID']."'>View</a></button></td></tr>"; /*When the button is clicked, it sends the guest ID of the row to guest_view.php*/
+                             <td><button  class='Offerbutton' name='ameneties'>Offer<br>Amenities</a></button>
+                                 <button class='Extendbutton' name='extend'>Extend<br>Stay</button>
+                                 <button type='submit' class='Checkoutbutton' name='checkout'>Early Checkout</button></td>
+                             <td><button class= 'Viewbutton'><a href= 'receptionist_guestview.php?id=".$rows['SID']."'>View</a></button></td></tr>
+                             <input type='hidden' name='guest_id' value='{$rows['guest_id']}'>
+                             <input type='hidden' name='bill_id' value='{$rows['bill_id']}'>
+                             <input type='hidden' name='fname' value='{$rows['fname']}'>
+                             <input type='hidden' name='lname' value='{$rows['lname']}'>
+                             <input type='hidden' name='mname' value='{$rows['mname']}'>
+                             
+
+
+
+
+                             </form>"; 
                       }
                     echo "</table>";
                 }else{
                   echo "No guest checked-in. ";
                 }
+
+         if(isset($_POST['extend'])){
+            $guest_id=$_POST['guest_id'];
+            $_SESSION['guest_id']=$guest_id;
+            $_SESSION['customer_id']=$customer_id;
+            $_SESSION['extend']="extend";
+            header("location:receptionist_update.php");
+            } 
+
+            if(isset($_POST['checkout'])){
+            $guest_id=$_POST['guest_id'];
+            $sched_id=$_POST['SID'];
+            $_SESSION['guest_id']=$guest_id;
+            $_SESSION['sched_id']=$sched_id;
+            header("location:receptionist_checkout.php");
+            }
+
+
+            if(isset($_POST['ameneties'])){
+            $guest_id=$_POST['guest_id'];
+            $bill_id=$_POST['bill_id'];
+            $fname=$_POST['fname'];
+            $mname=$_POST['mname'];
+            $lname=$_POST['lname'];
+
+            $_SESSION['guest_id']=$guest_id;
+            $_SESSION['bill_id']=$bill_id;
+            $_SESSION['fname']=$fname;
+            $_SESSION['mname']=$mname;
+            $_SESSION['lname']=$lname;
+
+
+
+
+
+
+            header("location:receptionist_ameneties.php");
+            }
+
+
+            ob_end_flush();
                $conn->close();        
         ?>
         </div>
