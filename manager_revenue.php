996eaa97ae63a57f1fcdf510b94bbe698faad05a
ai-ramen <73935314+ai-ramen@users.noreply.github.com>
2020-12-22 10:58:54 +0800

    Update manager_revenue.php

--- manager_revenue.php
+++ manager_revenue.php
@@ -10,18 +10,18 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>BookIT</title>
         <link rel="stylesheet" href="style.css">
-        <style type="text/css">
-            .grid-container {
-            display: grid;
-            grid-template-columns: auto auto;
-            grid-gap: 10px;
-            padding: 10px;
-            grid-auto-rows: minmax(200px, auto);
-            }
+    <style type="text/css">
+.grid-container {
+  display: grid;
+  grid-template-columns: auto auto;
+  grid-gap: 10px;
+  padding: 10px;
+  grid-auto-rows: minmax(200px, auto);
+}
 
-        </style>
+</style>
     </head>
-<body>
+    <body>
     <header>
         <div id="header">
         <img src="assets/bookIT_Logo.png">
@@ -61,52 +61,54 @@
                 <label for='year'>Year: </label>
                 <input type='number' name='year' id='yearly' min='2000' max='2021' required>
                 <input type='submit' name='search' value='Search'>
-                <input type='hidden' name='month' value='monthly'>
-                <input type='hidden' name='year' value='yearly'>
             </form>
+
             <?php
             include 'connection.php';
             //error_reporting(0);
+
             if(isset($_POST['search'])){
                 $month = $_POST['month'];
                 $year = $_POST['year'];
                 
-                echo "<div class='grid-container'>";
                 $week=date("W");
-                $sql3 = "SELECT COUNT(guest_id) as weekly
-                        FROM guests
-                        WHERE WEEK(date_in)=$week";
-                        $result3 = $conn->query($sql3);
-                        while($row3 = $result3->fetch_assoc()){
-                            echo "<button type='submit' name='select' style='background-color: #FEC200; padding: 10px;' class='button'><p>weekly</p><h1>".$row3['weekly']."</h1></button>";   
-                        }
-
-
-                    
-                $sql1 = "SELECT SUM(record_paid) AS 'monthly'
-                                
-                        FROM    records
-                        WHERE   MONTH(record_date) = '$month' AND
-                                record_type = 'CHECKED OUT'";
-                        $result1 = $conn->query($sql1);
-                        while($row1 = $result1->fetch_assoc()){
-                            echo "<button type='submit' name='select' style='background-color: #E35D40; padding: 10px;' class='button'><p>monthly</p><h1>".$row1['monthly']."</h1></button>";   
-                        }
 
                 
-                $sql2 = "SELECT SUM(record_paid) AS 'yearly'
-                                
-                        FROM    records
-                        WHERE   YEAR(record_date) = '$year' AND
-                        record_type = 'CHECKED OUT'";
-                        $result2 = $conn->query($sql2);
-                        while($row2 = $result2->fetch_assoc()){
-                            echo "<button type='submit' name='select' style='background-color: #C70039; padding: 10px; grid-column: 1 / span 2;' class='button'><p>yearly</p><h1>".$row2['yearly']."</h1></div>";   
-                        }                          
-                echo "</div>";
-            }
 
-            
+            echo "<div class='grid-container'>";
+        $week=date("W");
+        $sql3 = "SELECT SUM(payment_amount) as weekly
+                FROM payments
+                WHERE WEEK(payment_date)=$week";
+                $result3 = $conn->query($sql3);
+                while($row3 = $result3->fetch_assoc()){
+                    echo "<button type='submit' name='select' style='background-color: #FEC200; padding: 10px;' class='button'><p>weekly</p><h1>".$row3['weekly']."</h1></button>";   
+                }
+
+
+        
+        $sql1 = "SELECT SUM(payment_amount) as monthly
+                FROM payments
+                WHERE MONTH(payment_date)=$month";
+                $result1 = $conn->query($sql1);
+                while($row1 = $result1->fetch_assoc()){
+                    echo "<button type='submit' name='select' style='background-color: #E35D40; padding: 10px;' class='button'><p>monthly</p><h1>".$row1['monthly']."</h1></button>";   
+                }
+
+        $sql2 = "SELECT SUM(payment_amount) as yearly
+                FROM payments
+                WHERE YEAR(payment_date)=$year";
+                $result2 = $conn->query($sql2);
+                while($row2 = $result2->fetch_assoc()){
+                    echo "<button type='submit' name='select' style='background-color: #C70039; padding: 10px; grid-column: 1 / span 2;' class='button'><p>yearly</p><h1>".$row2['yearly']."</h1></div>";   
+                }
+
+        }
+                         
+        echo "</div>"
             ?>
+
+             
+
         </div>
-</body>
+    </body>
